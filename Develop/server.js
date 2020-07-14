a523d3bc821d10e8463e5cf397d68b3f407eff1a
Joe King <jking0186@gmail.com>
2020-07-14 15:34:47 -0500

    fixed css/js paths

--- Develop/server.js
+++ Develop/server.js
@@ -11,21 +11,21 @@ app.use(express.json())
 app.use(express.static("public"))
 app.use(morgan("dev"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 
 app.use(require("./routes/api"));
 
-app.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-})
+// app.get("/", function(req,res) {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// })
 
-app.get("/exercise", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"));
-})
+// app.get("/exercise", function(req,res) {
+//     res.sendFile(path.join(__dirname, "/public/exercise.html"));
+// })
 
-app.get("/stats", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/stats.html"));
-})
+// app.get("/stats", function(req,res) {
+//     res.sendFile(path.join(__dirname, "/public/stats.html"));
+// })
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
